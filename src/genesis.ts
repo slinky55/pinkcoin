0604b17462e1fbeea3d9a5407e9f98a698354e71
slinky55 <larry.mason@ufl.edu>
2023-04-02 00:26:07 -0400

    updated genesis block

--- src/genesis.ts
+++ src/genesis.ts
@@ -1,11 +1,12 @@
-import { Block, generateBlockHash } from "./block.js"
+import { Block } from "./block.js"
 
 export const GENESIS: Block = {
     timestamp: 0,
     lastHash: "",
     data: "genesis",
-    hash: generateBlockHash(0, "", "genesis", 0, 3),
-    nonce: 0,
+    hash: "12d7344a6d46a68d44611f823042f60b5e67cdef7af50ed14c21ac247281dae7",
     difficulty: 3,
-};
+    nonce: 0,
+}
+
 
